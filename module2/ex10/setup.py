c56eb5f1403cd0521a1cc67a7eada5e8b88cde87
whoto1 <118448487+whoto1@users.noreply.github.com>
2023-09-29 23:17:53 +0700

    Create setup.py

---
+++ module2/ex10/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'py_pubsub'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='alina',
+    maintainer_email='a.ten3@g.nsu.ru',
+    description='Examples of minimal publisher/subscriber using rclpy',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'text_to_cmd_vel = py_pubsub.text_to_cmd_vel:main',
+        ],
+    },
+)
