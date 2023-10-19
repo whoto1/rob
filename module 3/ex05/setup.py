5c839a6e443fc8ce889eacfe951d80c3c6905e88
whoto1 <118448487+whoto1@users.noreply.github.com>
2023-10-19 12:49:43 +0700

    Add files via upload

--- module 3/ex05/setup.py
+++ module 3/ex05/setup.py
@@ -1,1 +1,26 @@
+from setuptools import find_packages, setup
 
+package_name = 'move_to_goal'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='alina',
+    maintainer_email='a.ten3@g.nsu.ru',
+    description='Package for smth',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'move_to_goal = move_to_goal.move_to_goal:main'
+        ],
+    },
+)
